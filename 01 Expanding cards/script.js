b90d51298e08c7dd1314b5a75a7d5208ce354640
3ciadgr <201810394@fit.edu.ph>
2021-08-12 13:57:11 +0800

    added notes

--- 01 Expanding cards/script.js
+++ 01 Expanding cards/script.js
@@ -1,5 +1,7 @@
+// Selecting all panel classes
 const panels = document.querySelectorAll('.panel')
 
+//Adding active class in the panel class when clicked
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
@@ -7,6 +9,7 @@ panels.forEach(panel => {
     })
 })
 
+//Removing active class in the panel class when other panel class is clicked
 function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active')
